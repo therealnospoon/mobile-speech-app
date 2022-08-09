5e7b7d06c0a5594e485d8b89753cb11684972862
Tim Kim <timkimdesigns@gmail.com>
2022-08-08 17:21:29 -0700

    Updated icon component to utilize scale property to allow for micro-adjustments

--- app/components/icon.svelte
+++ app/components/icon.svelte
@@ -1,5 +1,7 @@
 <image
-    class="icon {customClass} {size}"
+    class="icon {customClass}"
+    data-size="{size}"
+    data-scale="{scale}"
     src="{path}"
 />
 
@@ -7,4 +9,5 @@
     export let path;
     export let customClass = "";
     export let size = "";
+    export let scale = ""; // Using this property scales up or scales down icon size by 5% depending on which value used ('up' or 'down');
 </script>
