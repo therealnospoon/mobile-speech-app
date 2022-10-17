d6f20baab196ecf14ffb07700e9c3d8c49b7e1f9
timkimdesigns <timkimdesigns@gmail.com>
2022-10-17 11:31:42 -0700

    updating gitignore

--- app/util/timer.ts
+++
@@ -1,5 +0,0 @@
-export const countdown = (func?:any, timelimit?:number) => {
-    const timeout = setTimeout(func, (timelimit * 1000));
-        
-    return timeout;
-};
--- types/shims.svelte.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "*.svelte" {
-    export {
-        SvelteComponent as default,
-    };
-}
